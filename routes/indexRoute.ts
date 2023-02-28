f561e90295cc58d923dd1b19d11e1b0be871d1ea
jerry <jerryfan71@gmail.com>
2023-02-27 16:30:53 -0800

    second update

--- routes/indexRoute.ts
+++ routes/indexRoute.ts
@@ -13,6 +13,26 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
 });
 
 
+router.get("/admin", async (req, res) => {
+  const storeSession = req.sessionStore;
+  const result: {id: number, sid: string}[] = []
+  storeSession.all?.((err, sessions: any) => {
+   for (const sid in sessions) {
+     const session = sessions[sid];
+     if (session.passport && session.passport.user) {
+      const userId = session.passport.user;
+      result.push({
+        id: userId,
+        sid: sid,
+      });
+      
+   }
+  }
+})
+    res.render("admin", {sessionData: result});
+});
+
+
 
 
 export default router;
